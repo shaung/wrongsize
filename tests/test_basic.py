5bca151adcac6643081169e5db842a4ed6fd042a
shaung <shaun.geng@gmail.com>
2013-04-11 20:42:09 +0900

    Add tests.

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import wrongsize as ws
+
+def test_db():
+    from wrongsize import db
+    db.inspect()
+
+if __name__ == '__main__':
+    test_db()
--- wrongsize/template.py
+++ wrongsize/template.py
@@ -1,4 +1,4 @@
-# coding: cp932
+# coding: utf-8
 
 import os
 import tempfile
